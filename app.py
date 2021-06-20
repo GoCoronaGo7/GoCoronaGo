92c1e34b571989a97165b1701add2d15d6669810
Shadopetro <shadopetro@gmail.com>
2021-06-20 23:14:25 +0530

    fixing navbar and removing redundant home.html

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def index():
     user = 'Not Logged in'
     if 'username' in session.keys():
         user = session['username']
-    return render_template('home.html')+'Hello World: Logged in as {user}'
-
+    return render_template('base.html')
 # Account methods
 
 
