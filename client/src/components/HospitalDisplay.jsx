e464f490f7d039beffdeca289c4c7db553acf523
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-08-09 11:56:34 +0530

    Added region filter
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- client/src/components/HospitalDisplay.jsx
+++ client/src/components/HospitalDisplay.jsx
@@ -9,7 +9,7 @@ const cache = new Map()
 
 export default function HospitalDisplay () {
     const [region, setRegion] = useState(STATS_DATA.state_names[15])
-    const [{ searchQuery, hospitals, filtered }, setSearchQuery] = useReducer(updateHospitals, { searchQuery: '', hospitals: null, filtered: null })
+    const [{ searchQuery, hospitals, filtered }, setSearchQuery] = useReducer(updateHospitals, { searchQuery: '', hospitals: null, filtered: null, region: null })
     const [error, setError] = useState('')
 
     useEffect(() => {
@@ -30,6 +30,7 @@ export default function HospitalDisplay () {
             < SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
             <div id="dropDown-bar">
                 < Dropdown options={STATS_DATA.state_names} value={region} onChange={({ value }) => setRegion(value)} />
+                < RegionDropDown hospitals={hospitals} setSearchQuery={setSearchQuery} />
             </div>
             < HospitalsDisplay hospitals={filtered} />
         </div>
@@ -42,18 +43,38 @@ function HospitalsDisplay ({ hospitals }) {
 HospitalsDisplay.propTypes = {
     hospitals: PropTypes.array
 }
+
+function RegionDropDown ({ hospitals, setSearchQuery }) {
+    const [regions, setRegion] = useState(null)
+    useEffect(() => {
+        setRegion(['All Regions', ...new Set(hospitals.values.map(x => x.area))])
+    }, [hospitals])
+    if (!hospitals || !regions) return <div id="loadingDisplay" style={{ display: 'grid', placeItems: 'center' }} > < LoadingIcon /> </div>
+    else return < Dropdown options={regions} value={'All Regions'} onChange={({ value }) => setSearchQuery({ type: 'region', region: value })}/>
+}
+RegionDropDown.propTypes = {
+    hospitals: PropTypes.object,
+    setSearchQuery: PropTypes.func.isRequired
+}
+
 function updateHospitals (state, action) {
     // update search Query
-    if (typeof action === 'string' || !action) return { ...state, searchQuery: action, filtered: filterHospitals(state.hospitals, action) }
-    else if (action instanceof Object && action.values && action.region) return { ...state, hospitals: action, filtered: filterHospitals(action, state.searchQuery) }
+    if (typeof action === 'string' || !action) return { ...state, searchQuery: action, filtered: filterHospitals(state.hospitals, action, state.region) }
+    else if (action instanceof Object && action.values && action.region) return { ...state, hospitals: action, filtered: filterHospitals(action, state.searchQuery, state.region) }
+    else if (action instanceof Object && action.type === 'region' && action.region) return { ...state, region: action.region, filtered: filterHospitals(state.hospitals, state.searchQuery, action.region) }
     else throw new Error()
 }
 
-function filterHospitals (hospitals, action) {
+function filterHospitals (hospitals, action, region) {
     if (!hospitals || hospitals.values?.length === 0) return []
-    if (!action || action === '') return hospitals.values
-    return hospitals.values.filter(x => x.hospital_name.toLowerCase().includes(action.toLowerCase()))
+    let filtered = hospitals.values
+    if (region && region !== 'All Regions') filtered = filtered.filter(x => x.area === region)
+    if (!action || action === '') return filtered
+
+    filtered = filtered.filter(x => x.hospital_name.toLowerCase().includes(action.toLowerCase()))
+    return filtered
 }
+
 async function getRegionDataByName (name) {
     const code = STATS_DATA.code_names[STATS_DATA.state_names.indexOf(name)]
     if (!code) return 'Invalid State Name'
