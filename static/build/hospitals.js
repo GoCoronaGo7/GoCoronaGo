1b6179622bb8db88dd2151f18f068f0da6414b0a
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-08-09 16:31:57 +0530

    moved build
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

---
+++ static/build/hospitals.js
@@ -0,0 +1,5 @@
+import HospitalDisplay from './components/HospitalDisplay.js';
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render( /*#__PURE__*/React.createElement(HospitalDisplay, null), document.getElementById('root'));
+});
+//# sourceMappingURL=hospitals.js.map
