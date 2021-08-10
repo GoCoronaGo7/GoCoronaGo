c8224a5f1aadce55f63690166f889a455a2befdc
Shadopetro <shadopetro@gmail.com>
2021-08-10 13:43:14 +0530

    Blog app

--- client/build/components/selector.js
+++ client/build/components/selector.js
@@ -1,26 +1,34 @@
 const {
-    useEffect
-} = React
-const buttons = ['Cases', 'Testing', 'Beds']
-export default props => {
-    const children = []
+  useEffect
+} = React;
+const buttons = ['Cases', 'Testing', 'Beds'];
+export default function Selector({
+  active,
+  setActive
+}) {
+  const children = [];
 
-    for (const el of buttons) {
-        children.push(/* #__PURE__ */React.createElement('button', {
-            key: `${el}`,
-            className: props.active.active == el ? 'active' : ''
-        }, el))
-    }
+  for (const el of buttons) {
+    children.push( /*#__PURE__*/React.createElement("button", {
+      key: el,
+      className: active === el ? 'active' : ''
+    }, el));
+  }
 
-    useEffect(() => {
-        const buttons = document.querySelectorAll('#selector > button')
-        buttons.forEach(button => {
-            button.addEventListener('click', e => {
-                if (button.classList.contains('active')) return; else props.active.setActive(button.innerHTML)
-            })
-        })
-    })
-    return /* #__PURE__ */React.createElement('div', {
-        id: 'selector'
-    }, children)
+  useEffect(() => {
+    const buttons = document.querySelectorAll('#selector > button');
+    buttons.forEach(button => {
+      button.addEventListener('click', e => {
+        if (!button.classList.contains('active')) setActive(button.innerHTML);
+      });
+    });
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    id: "selector"
+  }, children);
 }
+Selector.propTypes = {
+  active: PropTypes.string.isRequired,
+  setActive: PropTypes.func.isRequired
+};
+//# sourceMappingURL=Selector.js.map
