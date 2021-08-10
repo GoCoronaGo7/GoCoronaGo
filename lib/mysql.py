24114b8ac1084e0648f8e9eb6916ae86c6788818
Shadopetro <shadopetro@gmail.com>
2021-08-10 13:24:39 +0530

    Blog(functional wothout date function/ output needs to be beautified)

--- lib/mysql.py
+++ lib/mysql.py
@@ -1,4 +1,6 @@
 import mysql.connector as connector
+import datetime
+from datetime import datetime
 
 
 class Db:
@@ -39,12 +41,14 @@ class Db:
                     id INT NOT NULL AUTO_INCREMENT,
                     username varchar(25) NOT NULL,
                     content varchar(450) NOT NULL,
+                    date_post varchar(25) NOT NULL,
                     PRIMARY KEY (id) 
-                    )
-        '''
+                    )         
+        ''' 
         cursor.execute(query_user_setup)
         cursor.execute(query_doc_setup)
         cursor.execute(query_blog_setup)
+        
     def get_cursor(self):
         try:
             return self.connection.cursor()
@@ -88,10 +92,10 @@ class Db:
             f'''INSERT INTO `user` (username,password,email) values('{user}', '{passw}', '{email}')''')
         return self.connection.commit()
     
-    def insert_blog(self,user,content):
+    def insert_blog(self,user,content,date):
         cursor = self.get_cursor()
         cursor.execute(
-            f'''INSERT INTO `blog` (username,content) values('{user}', '{content}')''')
+            f'''INSERT INTO `blog` (username,content,date_post) values('{user}', '{content}',{date})''')
         return self.connection.commit()
     
     def get_blog(self):
@@ -100,6 +104,16 @@ class Db:
             f'''SELECT * FROM `blog` ''')
         blogout=cursor.fetchall()
         return blogout
+    
+    def check_blog(self,Blog_date_check):
+        cursor = self.get_cursor()
+        cursor.execute(
+            f'''SELECT `date_post` FROM `blog` where `id`=1 ''')
+        blog_post_date = cursor.fetchone
+        if Blog_date_check != blog_post_date:
+            cursor.execute(
+                f'''DROP TABLE `blog` ''')
+        return self.connection.commit()
     
     def update(self, user, **kwargs):
         print(kwargs)
@@ -117,5 +131,3 @@ class Db:
         for (key, value) in args.items():
             query += f'{key}={value}'
         return query
-        
-    
