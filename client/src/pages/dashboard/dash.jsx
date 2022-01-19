3806cfa65e5ebdf33e836462dd6e5e1e6819fa29
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-01-19 10:42:19 +0530

    Added patient-doctors window
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- client/src/pages/dashboard/dash.jsx
+++ client/src/pages/dashboard/dash.jsx
@@ -1,45 +1,51 @@
 const { useEffect } = React
 
 // eslint-disable-next-line react/prop-types
-export default function Dash ({ admin, sideBar, children }) {
+export default function Dash ({ doctors, admin, sideBar, children }) {
     return <div id="dash">
-        { sideBar }
+        {sideBar}
         { children }
     </div>
 }
 
-// eslint-disable-next-line react/prop-types
+Dash.propTypes = {
+    sideBar: PropTypes.component
+}
+
 export function SideBar ({ admin, options, active, setActive }) {
     useEffect(() => {
         const buttons = document.querySelectorAll('#sideBarButton')
         buttons.forEach((button, i) => {
             button.addEventListener('click', (e) => {
-                console.log(i)
                 setActive(i)
             })
         })
     }, [setActive])
-    console.log(options)
-    return <div className="sideBar">
-        <div className="profileHolder">
-            {admin
-                ? <>
-                    <div id="profilePic"> <object data="/static/images/admin.svg" aria-label="admin" /> </div>
-                    <h2> Dr.{ FLASK_SESSION.username }</h2>
-                    <span> Doctor </span>
-                </>
-                : <>
-                    <div id="profilePic"> <object data="/static/images/admin.svg" aria-label="admin" /> </div>
-                    <h2> { FLASK_SESSION.username }</h2>
-                    <span> Patient </span>
-                </>
-            }
 
+    return <>
+        <div className="sideBar">
+            <div className="profileHolder">
+                {admin
+                    ? <>
+                        <div id="profilePic"> <object data="/static/images/admin.svg" aria-label="admin" /> </div>
+                        <h2> Dr.{ FLASK_SESSION.username }</h2>
+                        <span> Doctor </span>
+                    </>
+                    : <>
+                        <div id="profilePic"> <object data="/static/images/admin.svg" aria-label="admin" /> </div>
+                        <h2> { FLASK_SESSION.username }</h2>
+                        <span> Patient </span>
+                    </>
+                }
+            </div>
+            {options.map(([title, icon], i) => <button id="sideBarButton" className={'sideBarButton' + ((i === active) ? ' active' : '')} key={i} ><object aria-label={title} data={icon} type="image/svg+xml" /> {title}</button>)}
         </div>
-        {options.map(([title, icon], i) => <button id="sideBarButton" className={'sideBarButton' + ((i === active) ? ' active' : '')} key={i} ><object aria-label={title} data={icon} type="image/svg+xml" /> {title}</button>)}
-    </div>
+    </>
 }
 
 SideBar.propTypes = {
+    admin: PropTypes.boolean,
+    active: PropTypes.number,
+    setActive: PropTypes.func,
     options: PropTypes.array
 }
--- client/src/pages/dashboard/userDash.jsx
+++ client/src/pages/dashboard/userDash.jsx
@@ -1,9 +1,19 @@
+import Doctors from './subPages/Doctors.js'
+
 import Dash, { SideBar } from './dash.js'
-const { useState } = React
+const { useState, useMemo } = React
 
 export default function UserDash () {
     const [active, setActive] = useState(0)
-
-    const sideBar = <SideBar options={[['Doctors', '/static/images/doctor.svg']]}{...{ active, setActive }}/>
-    return <Dash {...{ sideBar }}></Dash>
+    console.log(window.doctors, window.requests)
+    const sideBar = <SideBar options={[['Doctors', '/static/images/doctor.svg']]}{...{ active, setActive }} />
+    const content = useMemo(() => {
+        switch (active) {
+        case 0:
+            return <Doctors doctors={window.doctors} />
+        default:
+            return <></>
+        }
+    }, [active])
+    return <Dash {...{ doctors: window.doctors, sideBar }}> {content} </Dash>
 }
