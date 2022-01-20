3d775217db6229080a285224a3f64fd74ab7a3ce
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-01-20 18:34:57 +0530

    paginate doctors
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    presets: ['@babel/preset-react', 'minify'],
+    presets: ['@babel/preset-react', ['minify', { builtIns: false }]],
     plugins: [
-        ['transform-react-remove-prop-types',
+        [
+            'transform-react-remove-prop-types',
             {
                 mode: 'remove'
-
             }
         ]
     ]
