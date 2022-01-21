abf97c9ef6bdafbc0af98893f2e5e795a10405de
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-01-21 23:36:49 +0530

    added admin dash
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- static/build/pages/dashboard/adminDash.js
+++ static/build/pages/dashboard/adminDash.js
@@ -1,9 +1,11 @@
 import Dash, { SideBar } from './dash.js';
+import Patients from './subPages/Patients.js';
 const {
   useState
 } = React;
 export default function AdminDash() {
   const [active, setActive] = useState(0);
+  const patients = window.patients;
   const sideBar = /*#__PURE__*/React.createElement(SideBar, {
     admin: true,
     options: [['Patients', '/static/images/doctor.svg']],
@@ -13,6 +15,8 @@ export default function AdminDash() {
   return /*#__PURE__*/React.createElement(Dash, {
     admin: true,
     sideBar
-  });
+  }, " ", /*#__PURE__*/React.createElement(Patients, {
+    patients: patients
+  }), " ");
 }
 //# sourceMappingURL=adminDash.js.map
