a43a0b0032a6edfb2421daf65c2bebb84a77341f
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-01-24 23:33:12 +0530

    minify
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- static/build/dashboard.js
+++ static/build/dashboard.js
@@ -1,11 +1,1 @@
-import AdminDash from './pages/dashboard/adminDash.js';
-import UserDash from './pages/dashboard/userDash.js';
-if (!FLASK_SESSION.loggedin) window.location = '/login';
-document.addEventListener('DOMContentLoaded', function () {
-  ReactDOM.render( /*#__PURE__*/React.createElement(Dash, null), document.getElementById('root'));
-});
-
-const Dash = () => {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, FLASK_SESSION.admin ? /*#__PURE__*/React.createElement(AdminDash, null) : /*#__PURE__*/React.createElement(UserDash, null));
-};
-//# sourceMappingURL=dashboard.js.map
+import AdminDash from"./pages/dashboard/adminDash.js";import UserDash from"./pages/dashboard/userDash.js";FLASK_SESSION.loggedin||(window.location="/login"),document.addEventListener("DOMContentLoaded",function(){ReactDOM.render(/*#__PURE__*/React.createElement(Dash,null),document.getElementById("root"))});const Dash=()=>/*#__PURE__*/React.createElement(React.Fragment,null,FLASK_SESSION.admin?/*#__PURE__*/React.createElement(AdminDash,null):/*#__PURE__*/React.createElement(UserDash,null));
